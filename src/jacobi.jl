b80fc1c4edb5317bd60589463b013a60623dac7a
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-06-15 20:32:57 +0100

    completed jacobi tests

--- src/jacobi.jl
+++ src/jacobi.jl
@@ -6,7 +6,9 @@
 # --------------------------------------------------------------------------
 
 
-module Jacobi
+module JacobiPolys
+
+export Jacobi, eval_basis, eval_basis!, eval_grad, eval_grad!
 
 """
 `Jacobi{T} : ` represents the basis of Jacobi polynomials
@@ -45,29 +47,36 @@ function Jacobi(α, β, N, T=Float64)
       c1 = big(2*n*(n+α+β)*(2*n+α+β-2))
       c2 = big(2*n+α+β-1)
       A[n] = T( big(2*n+α+β)*big(2*n+α+β-2)*c2 / c1 )
-      B[n] = T( big(α^2 - β^2) * c2 / c1) )
+      B[n] = T( big(α^2 - β^2) * c2 / c1 )
       C[n] = T( big(-2*(n+α-1)*(n+β-1)*(2n+α+β)) / c1 )
    end
    return Jacobi(α, β, A, B, C)
 end
 
-function eval_basis!(P::AbstractVector, J::Jacobi, x::Number, N::Integer)
+function eval_basis!(P::AbstractVector, J::Jacobi, x,
+                     N::Integer = length(P)-1 )
    @assert length(P) >= N+1
    @assert 0 <= N <= length(J.A)
    α, β = J.α, J.β
    P[1] = 1
-   if N >= 1
-      P[2] = (α+1) + 0.5 * (α+β+2) * (x-1)
-   end
+   if N >= 1; P[2] = (α+1) + 0.5 * (α+β+2) * (x-1); end
    for n = 2:N
       P[n+1] = (J.A[n] * x + J.B[n]) * P[n] + J.C[n] * P[n-1]
    end
    return P
 end
 
+eval_basis(J::Jacobi, x::Number, N::Integer, T=Float64) =
+      eval_basis!(zeros(T, N+1), J, x, N)
+
+eval_basis(J, x::Number, T=Float64) =
+      eval_basis(J, x, length(J.A), T)
+
+
 function eval_grad!(P::AbstractVector, dP::AbstractVector,
-                    J::Jacobi, x::Number, N::Integer)
+                    J::Jacobi, x::Number, N::Integer = length(P)-1)
    @assert length(P) >= N+1
+   @assert length(dP) >= N+1
    @assert 0 <= N <= length(J.A)
    α, β = J.α, J.β
    P[1] = 1
@@ -84,6 +93,12 @@ function eval_grad!(P::AbstractVector, dP::AbstractVector,
    end
    return P, dP
 end
+
+eval_grad(J::Jacobi, x::Number, N::Integer, T=Float64) =
+      eval_grad!(zeros(T, N+1), zeros(T, N+1), J, x, N)
+
+eval_grad(J, x::Number, T=Float64) =
+      eval_grad(J, x, length(J.A), T)
 
 
 end
---
+++ test/test_jacobi.jl
@@ -0,0 +1,61 @@
+
+# --------------------------------------------------------------------------
+# ACE.jl and SHIPs.jl: Julia implementation of the Atomic Cluster Expansion
+# Copyright (c) 2019 Christoph Ortner <christophortner0@gmail.com>
+# All rights reserved.
+# --------------------------------------------------------------------------
+
+
+@testset "Jacobi" begin
+
+using SHIPs, Test
+
+using SHIPs.JacobiPolys: Jacobi, eval_basis, eval_grad
+
+# copy-pasted from
+# https://github.com/pjabardo/Jacobi.jl/blob/master/src/jac_poly.jl
+# commit 9642d8f060203ddddeb17484a3aa8232022c7ba5
+
+function jacobi(x, n, a, b)
+   ox = one(x)
+   zx = zero(x)
+   if n==0
+     return ox
+   elseif n==1
+     return ox/2 * (a - b + (a + b + 2)*x)
+   end
+
+   p0 = ox
+   p1 = ox/2 * (a - b + (a + b + 2)*x)
+   p2 = zx;
+
+   for i = 1:(n-1)
+      a1 = 2*(i+1)*(i+a+b+1)*(2*i+a+b);
+      a2 = (2*i+a+b+1)*(a*a-b*b);
+      a3 = (2*i+a+b)*(2*i+a+b+1)*(2*i+a+b+2);
+      a4 = 2*(i+a)*(i+b)*(2*i+a+b+2);
+      p2 = ox/a1*( (a2 + a3*x)*p1 - a4*p0);
+
+      p0 = p1
+      p1 = p2
+   end
+
+   return p2
+end
+
+djacobi(x, n, a, b) =  one(x)/2 * (a + b + n + 1) * jacobi(x, n-1, a+1, b+1)
+
+for ntest = 1:30
+   x = 2*rand() - 1
+   α, β = rand(), rand()
+   N = 30
+   P = eval_basis(Jacobi(α, β, N), x)
+   P1, dP = eval_grad(Jacobi(α, β, N), x)
+   Ptest = [ jacobi(x, n, α, β) for n = 0:N ]
+   dPtest = [ djacobi(x, n, α, β) for n = 0:N ]
+   print((@test P ≈ P1 ≈ Ptest), " ")
+   print((@test dP ≈ dPtest), " ")
+end
+println()
+
+end
