37834364cd12ae9d40904e0f73466817d87cbd83
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-06-16 14:19:13 +0100

    draft transformed Jacobi

--- src/SHIPs.jl
+++ src/SHIPs.jl
@@ -1,13 +1,10 @@
 module SHIPs
 
+include("prototypes.jl")
 include("jacobi.jl")
-
-include("sphericalharmonics.jl") 
-
+include("sphericalharmonics.jl")
+include("transforms.jl")
 include("basis.jl")
-
 include("calculators.jl")
-
-
 
 end # module
---
+++ src/prototypes.jl
@@ -0,0 +1,12 @@
+
+
+function eval_basis end
+function eval_basis! end
+function eval_grad end
+function eval_basis_d! end
+
+
+function transform end
+function transform_d end
+function fcut end
+function fcut_d end
