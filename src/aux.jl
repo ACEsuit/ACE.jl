8d9365577f1b149cd1e76f8174940301d0d9db75
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-06-17 20:59:36 +0100

    first draft of the SHIPBasis

--- src/aux.jl
+++ src/aux.jl
@@ -6,98 +6,22 @@
 # --------------------------------------------------------------------------
 
 
-"""
-Number of polynomials of total degree d with weights w, i.e. the number of
-tuples (k1,..., kn) ∈ ℕⁿ where `n = length(w)` such that k ⋅ w ≦ d
-"""
-_npoly_tot(d, w::Vector) = isempty(w) ? 1 : (
-            sum( _npoly_tot(d - w[1]*n, w[2:end]) for n = 0:floor(Int,d/w[1]) )
-      )
-
-Main._npoly_tot(10, [1,1,1,1,2,2,2,2]) / factorial(4)
 
 
 
-# function generate_tuples(deg, wY::Real, bo)
-#    # all possible (k, l) pairs
-#    allKL = NamedTuple{(:k, :l, :deg),Tuple{Int64,Int64,Int64}}[]
-#    # k + wY * l <= deg
-#    for k = 0:deg, l = 0:floor(Int, (deg-k)/wY)
-#       push!(allKL, (k=k, l=l, deg=(k+wY*l)))
-#    end
-#    allKL = allKL[1:6]
+# allKL, Nu = generate_tuples(12, 2, 8)
+# display(allKL)
+# length(Nu)
 #
-#    # the first iterm is just (0, ..., 0)
-#    # we can choose (k1, l1), (k2, l2) ... by indexing into allKL
-#    Nu = ones(Int, 1, bo)  # [1, ..., 1]
-#    _deg(ν) = sum( allKL[n].deg for n in ν )
-#    # Now we start incrementing until we hit the maximum degree
-#    # while retaining the ordering ν₁ ≤ ν₂ ≤ …
-#    curidx = bo
-#    ν = Nu[end,:]
-#    while true
-#       # we want to increment `curindex`, but if we've reach the maximum degree
-#       # then we need to move to the next index down
-#       if _deg(ν) >= deg
-#          if curidx == 1
-#             # we are done! stop the while loop
-#             break
-#          end
-#          ν[curidx:end] .= ν[curidx-1] + 1
-#          curidx -= 1
-#       else
-#          ν[curidx] += 1
-#          Nu = [Nu; ν']
-#          curidx = bo
-#       end
-#    end
-#    return allKL, Nu
+# @info("Degree 12")
+# for bo = 3:10
+#    allKL, Nu = generate_tuples(12, 2, bo)
+#    @show bo, length(Nu)
 # end
 #
+# @info("Degree 15")
+# for bo = 3:10
+#    allKL, Nu = generate_tuples(15, 2, bo)
+#    @show bo, length(Nu)
+# end
 #
-# allKL, Nu = generate_tuples(5, 1, 3)
-# display(allKL)
-# display(Nu)
-
-
-
-function generate_tuples(deg, wY::Real, bo)
-   # all possible (k, l) pairs
-   allKL = NamedTuple{(:k, :l, :deg),Tuple{Int64,Int64,Int64}}[]
-   # k + wY * l <= deg
-   for k = 0:deg, l = 0:floor(Int, (deg-k)/wY)
-      push!(allKL, (k=k, l=l, deg=(k+wY*l)))
-   end
-   allKL = allKL[1:6]
-
-   # the first iterm is just (0, ..., 0)
-   # we can choose (k1, l1), (k2, l2) ... by indexing into allKL
-   Nu = ones(Int, 1, bo)  # [1, ..., 1]
-   _deg(ν) = sum( allKL[n].deg for n in ν )
-   # Now we start incrementing until we hit the maximum degree
-   # while retaining the ordering ν₁ ≤ ν₂ ≤ …
-   curidx = bo
-   ν = Nu[end,:]
-   while true
-      # we want to increment `curindex`, but if we've reach the maximum degree
-      # then we need to move to the next index down
-      if _deg(ν) >= deg
-         if curidx == 1
-            # we are done! stop the while loop
-            break
-         end
-         ν[curidx:end] .= ν[curidx-1] + 1
-         curidx -= 1
-      else
-         ν[curidx] += 1
-         Nu = [Nu; ν']
-         curidx = bo
-      end
-   end
-   return allKL, Nu
-end
-
-
-allKL, Nu = generate_tuples(5, 1, 3)
-display(allKL)
-display(Nu)
--- src/basis.jl
+++ src/basis.jl
@@ -12,12 +12,72 @@ using JuLIP
 
 using SHIPs.SphericalHarmonics: SHBasis, sizeY, SVec3
 
-struct SHIPBasis{BO, TJ, TSH}
+# -------------------------------------------------------------
+#       construct l,k tuples that specify basis functions
+# -------------------------------------------------------------
+
+function generate_LK_tuples(deg, wY::Real, bo)
+   # all possible (k, l) pairs
+   allKL = NamedTuple{(:k, :l, :deg),Tuple{Int,Int,Float64}}[]
+   degs = Int[]
+   # k + wY * l <= deg
+   for k = 0:deg, l = 0:floor(Int, (deg-k)/wY)
+      push!(allKL, (k=k, l=l, deg=(k+wY*l)))
+      push!(degs, (k+wY*l))
+   end
+   # sort allKL according to total degree
+   I = sortperm(degs)
+   allKL = allKL[I]
+   degs = degs[I]
+
+   # the first iterm is just (0, ..., 0)
+   # we can choose (k1, l1), (k2, l2) ... by indexing into allKL
+   Nu = []
+   _deg(ν) = maximum(ν) <= length(allKL) ? sum( allKL[n].deg for n in ν ) : Inf
+   # Now we start incrementing until we hit the maximum degree
+   # while retaining the ordering ν₁ ≤ ν₂ ≤ …
+   lastidx = 0
+   ν = MVector(ones(Int, bo)...)
+   ctr = 1000
+   while true
+      # we want to increment `curindex`, but if we've reach the maximum degree
+      # then we need to move to the next index down
+
+      # if the current tuple ν has admissible degree ...
+      if _deg(ν) <= deg
+         # ... then we add it to the stack  ...
+         push!(Nu, SVector(ν))
+         # ... and increment it
+         lastidx = bo
+         ν[lastidx] += 1
+      else
+         # we have overshot, _deg(ν) > deg; we must go back down, by
+         # decreasing the index at which we increment
+         if lastidx == 1
+            break
+         end
+         ν[lastidx-1:end] .= ν[lastidx-1] + 1
+         lastidx -= 1
+      end
+   end
+   return allKL, Nu
+end
+
+
+# -------------------------------------------------------------
+#       define the basis itself
+# -------------------------------------------------------------
+
+
+struct SHIPBasis{BO, T, TJ, TSH}
    deg::Int
-   wY::Float64
+   wY::T
    J::TJ
    SH::TSH
-   A::Vector{Float64}
+   A::Vector{Complex{T}}
+   dA::Vector{SVec3{Complex{T}}}
+   allKL::NamedTuple{(:k, :l, :deg),Tuple{Int,Int,T}}
+   Nu::Vector{SVector{BO, Int}}
    valBO::Val{BO}
 end
 
@@ -26,13 +86,6 @@ length_A(deg, wY) = sum( sizeY( floor(Int, (deg - k)/wY) ) for k = 0:deg )
 # this could become and allox_temp
 alloc_A(deg, wY) = zeros(ComplexF64, length_A(deg, wY))
 alloc_dA(deg, wY) = zeros(SVec3{ComplexF64}, length_A(deg, wY))
-
-
-length_B(bo, deg, wY) = Main._npoly_tot(deg, [ ones(bo-1); wY * ones(bo-1) ])
-length_B(ship::SHIPBasis{BO}) where {BO} = length_B(BO, ship.deg, ship.wY)
-
-alloc_B(ship::SHIPBasis) = zeros(Float64, length_B(ship))
-alloc_dB(ship::SHIPBasis) = zeros(SVec3{Float64}, length_B(ship))
 
 
 
@@ -44,12 +97,26 @@ function SHIPBasis(bo::Integer, deg::Integer, wY::Real, trans, p, rl, ru)
    maxL = floor(Int, deg / wY)
    SH = SHBasis(maxL)
    # allocate space for the A array
-   A = Vector{Float64}(undef, length_A(deg, wY))
-
+   A = alloc_A(deg, wY)
+   dA = alloc_B(deg, wY)
+   # get the basis specification
+   allKL, Nu = generate_LK_tuples(deg, wY, bo)
    # precompute the Clebsch-Gordan coefficients
+   # TODO: maybe later ...
+   # putting it all together ...
+   return SHIPBasis(deg, wY, J, SH, A, dA, allKL, Nu, Val(bo))
 end
 
 
-
-
 bodyorder(ship::SHIPBasis{BO}) where {BO} = BO
+
+length_B(ship::SHIPBasis{BO}) where {BO} = length(ship.Nu)
+
+alloc_B(ship::SHIPBasis) = zeros(Float64, length_B(ship))
+alloc_dB(ship::SHIPBasis) = zeros(SVec3{Float64}, length_B(ship))
+
+
+
+# -------------------------------------------------------------
+#       precompute the J, SH and A arrays
+# -------------------------------------------------------------
