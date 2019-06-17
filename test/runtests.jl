bc87f063c56feba42b9451199e88727e2ea85530
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-06-17 11:36:53 +0100

    all derivative tests passing!

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using SHIPs
-using Test
+using Test, Printf, LinearAlgebra, StaticArrays, BenchmarkTools, Test
 
 @testset "SHIPs.jl" begin
     include("test_jacobi.jl")
+    include("test_transforms.jl")
     include("test_ylm.jl")
     include("test_cg.jl")
 end
