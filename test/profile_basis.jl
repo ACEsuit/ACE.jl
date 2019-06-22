8bab60cf470be9135f91277a905146ac9bd76e3d
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-06-22 09:06:08 +0100

    profiling code

--- test/profile_basis.jl
+++ test/profile_basis.jl
@@ -18,39 +18,39 @@ Rs = 1.0 .+ rand(JVecF, 50)
 @btime SHIPs.precompute_A!($(ships[1]), $Rs)
 @btime SHIPs.precompute_A!($(ships[1]), $Rs)
 
-# @info("profile basis computation")
-# for n = 2:4
-#    @info("  body-order $(n+1):")
-#    🚢 = ships[n-1]
-#    B = SHIPs.alloc_B(🚢)
-#    @info("     eval_basis:")
-#    @btime SHIPs.eval_basis!($B, $🚢, $Rs)
-#    @btime SHIPs.eval_basis!($B, $🚢, $Rs)
-#    @info("     eval_basis_d:")
-#    dB = SHIPs.alloc_dB(🚢, Rs)
-#    store = SHIPs.alloc_temp_d(🚢, Rs)
-#    @btime SHIPs.eval_basis_d!($B, $dB, $🚢, $Rs, $store)
-#    @btime SHIPs.eval_basis_d!($B, $dB, $🚢, $Rs, $store)
-# end
-
-##
-using Profile
-🚢 = ships[1]
-B = SHIPs.alloc_B(🚢)
-dB = SHIPs.alloc_dB(🚢, Rs)
-store = SHIPs.alloc_temp_d(🚢, Rs)
-@btime SHIPs.eval_basis_d!($B, $dB, $🚢, $Rs, $store)
-
-##
-
-function runn(N, args...)
-   for n = 1:N
-      SHIPs.eval_basis_d!(args...)
-   end
+@info("profile basis computation")
+for n = 2:4
+   @info("  body-order $(n+1):")
+   🚢 = ships[n-1]
+   B = SHIPs.alloc_B(🚢)
+   @info("     eval_basis:")
+   @btime SHIPs.eval_basis!($B, $🚢, $Rs)
+   @btime SHIPs.eval_basis!($B, $🚢, $Rs)
+   @info("     eval_basis_d:")
+   dB = SHIPs.alloc_dB(🚢, Rs)
+   store = SHIPs.alloc_temp_d(🚢, Rs)
+   @btime SHIPs.eval_basis_d!($B, $dB, $🚢, $Rs, $store)
+   @btime SHIPs.eval_basis_d!($B, $dB, $🚢, $Rs, $store)
 end
-runn(2, B, dB, 🚢, Rs, store)
 
-Profile.clear()
-@profile runn(10_000,  B, dB, 🚢, Rs, store)
-
-Profile.print()
+# ##
+# using Profile
+# 🚢 = ships[1]
+# B = SHIPs.alloc_B(🚢)
+# dB = SHIPs.alloc_dB(🚢, Rs)
+# store = SHIPs.alloc_temp_d(🚢, Rs)
+# @btime SHIPs.eval_basis_d!($B, $dB, $🚢, $Rs, $store)
+#
+# ##
+#
+# function runn(N, args...)
+#    for n = 1:N
+#       SHIPs.eval_basis_d!(args...)
+#    end
+# end
+# runn(2, B, dB, 🚢, Rs, store)
+#
+# Profile.clear()
+# @profile runn(10_000,  B, dB, 🚢, Rs, store)
+#
+# Profile.print()
--- test/profile_ship.jl
+++ test/profile_ship.jl
@@ -5,3 +5,44 @@
 # All rights reserved.
 # --------------------------------------------------------------------------
 
+
+using SHIPs, JuLIP, BenchmarkTools, LinearAlgebra
+
+function randR()
+   R = rand(JVecF) .- 0.5
+   return (0.9 + 2 * rand()) * R/norm(R)
+end
+randR(N) = [ randR() for n=1:N ]
+randcoeffs(B) = rand(length(B)) .* (1:length(B)).^(-2)
+
+trans = PolyTransform(2, 1.0)
+BB = [SHIPBasis(n, 15, 2.0, trans, 2, 0.5, 3.0) for n = 2:4]
+
+Rs = randR(50)
+@info("profile precomputation of A")
+@btime SHIPs.precompute_A!($(BB[1]), $Rs)
+@btime SHIPs.precompute_A!($(BB[1]), $Rs)
+
+@info("profile basis and ship computation")
+for n = 2:4
+   @info("  body-order $(n+1):")
+   Rs = randR(50)
+   B = BB[n-1]
+   coeffs = randcoeffs(B)
+   🚢 = SHIP(B, coeffs)
+   b = SHIPs.alloc_B(B)
+   store = SHIPs.alloc_temp(🚢)
+   @info("     evaluate a site energy:")
+   print("         SHIPBasis: "); @btime SHIPs.eval_basis!($b, $B, $Rs, nothing)
+   print("         SHIP     : "); @btime SHIPs.evaluate!($🚢, $Rs, $store)
+
+   store = SHIPs.alloc_temp_d(🚢, Rs)
+   dEs = zeros(JVecF, length(Rs))
+   db = SHIPs.alloc_dB(B, Rs)
+   dbtmp = SHIPs.alloc_temp_d(B, Rs)
+
+   @info("     site energy gradient:")
+   store = SHIPs.alloc_temp_d(🚢, Rs)
+   print("         SHIPBasis: "); @btime SHIPs.eval_basis_d!($b, $db, $B, $Rs, $dbtmp)
+   print("         SHIP     : "); @btime SHIPs.evaluate_d!($dEs, $🚢, $Rs, $store)
+end
