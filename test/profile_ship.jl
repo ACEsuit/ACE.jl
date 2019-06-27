5b5d41d60d85924da517ab29fadce17cbe05e629
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-06-27 15:22:40 +0100

    fixing SHIP profiling

--- test/profile_ship.jl
+++ test/profile_ship.jl
@@ -16,27 +16,32 @@ randR(N) = [ randR() for n=1:N ]
 randcoeffs(B) = rand(length(B)) .* (1:length(B)).^(-2)
 
 trans = PolyTransform(2, 1.0)
-BB = [SHIPBasis(n, 15, 2.0, trans, 2, 0.5, 3.0) for n = 2:4]
+BB = [ SHIPBasis(2, 20, 1.0, trans, 2, 0.5, 3.0),
+       SHIPBasis(3, 20, 1.5, trans, 2, 0.5, 3.0),
+       SHIPBasis(4, 15, 1.5, trans, 2, 0.5, 3.0) ]
 
-Rs = randR(50)
+Nat = 50
+Rs = randR(Nat)
+btmp = SHIPs.alloc_temp(BB[1])
 @info("profile precomputation of A")
-@btime SHIPs.precompute_A!($(BB[1]), $Rs)
-@btime SHIPs.precompute_A!($(BB[1]), $Rs)
+@btime SHIPs.precompute_A!($btmp, $(BB[1]), $Rs)
+# @btime SHIPs.precompute_A!($(BB[1]), $Rs, $btmp)
 
 @info("profile basis and ship computation")
 for n = 2:4
    @info("  body-order $(n+1):")
-   Rs = randR(50)
+   Rs = randR(Nat)
    B = BB[n-1]
    coeffs = randcoeffs(B)
    🚢 = SHIP(B, coeffs)
    b = SHIPs.alloc_B(B)
-   store = SHIPs.alloc_temp(🚢)
+   btmp = SHIPs.alloc_temp(B)
+   tmp = SHIPs.alloc_temp(🚢)
    @info("     evaluate a site energy:")
-   print("         SHIPBasis: "); @btime SHIPs.eval_basis!($b, $B, $Rs, nothing)
-   print("         SHIP     : "); @btime SHIPs.evaluate!($🚢, $Rs, $store)
+   print("         SHIPBasis: "); @btime SHIPs.eval_basis!($b, $B, $Rs, $tmp)
+   print("         SHIP     : "); @btime SHIPs.evaluate!($🚢, $Rs, $tmp)
 
-   store = SHIPs.alloc_temp_d(🚢, Rs)
+   tmp = SHIPs.alloc_temp_d(🚢, Rs)
    dEs = zeros(JVecF, length(Rs))
    db = SHIPs.alloc_dB(B, Rs)
    dbtmp = SHIPs.alloc_temp_d(B, Rs)
