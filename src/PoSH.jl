e86d9ad6e27a3fafb83237b012e40385f60149ef
Christoph Ortner <c.ortner@warwick.ac.uk>
2020-02-04 14:22:51 +0000

    finished first implementation + testing of OrthogonalPolys

--- src/PoSH.jl
+++ src/PoSH.jl
@@ -15,7 +15,7 @@ include("pairpots/basis.jl")
 include("pairpots/calculator.jl")
 include("pairpots/repulsion.jl")
 
-# include("pairpots/orthpolys.jl")
+include("pairpots/orthpolys.jl")
 
 # specification of the angular basis
 include("sphericalharmonics.jl")
@@ -52,7 +52,8 @@ include("utils.jl")
 #       include("convertc2r.jl")
 #    end
 # end
-
+#  TODO: make conertc2r load only conditionally. The above seems to be
+#        incorrect, probably used Requires incorrectly? 
 include("extras/convertc2r.jl")
 
 end # module
