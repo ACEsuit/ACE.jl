ea688f5f03310a0807a9d52fb074f96483fa860e
Christoph Ortner <c.ortner@warwick.ac.uk>
2020-05-14 22:34:52 -0700

    random potentials and preconditioners; ready to start serious testing!

--- src/SHIPs.jl
+++ src/SHIPs.jl
@@ -34,7 +34,12 @@ include("rpi/rpi.jl")
 include("pairpots/pair.jl");
 @reexport using SHIPs.PairPotentials
 
-
+# lots of stuff related to random samples:
+#  - random configurations
+#  - random potentials
+#  ...
+include("random.jl")
+@reexport using SHIPs.Random 
 
 # - bond model
 # - pure basis
