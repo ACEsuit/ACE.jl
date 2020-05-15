6b0ec167f92d66f094289ed9f79b644e763da34b
Christoph Ortner <c.ortner@warwick.ac.uk>
2020-05-15 16:19:15 -0700

    cleanup

--- src/utils.jl
+++ src/utils.jl
@@ -9,10 +9,16 @@
 module Utils
 
 # - simple ways to construct a radial basis
-# -
+# - construct a descriptor
+# - simple wrappers to generate RPI basis functions (ACE + relatives) 
 
 function rpi_basis(; species = :X)
 
-end 
+end
+
+
+function descriptor()
+
+end
 
 end
