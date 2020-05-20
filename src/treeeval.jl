5aefba9860c3db811f9646d2ed6ca3539e235250
Christoph Ortner <c.ortner@warwick.ac.uk>
2020-05-20 10:21:18 -0700

    construction and evaluation drafted. Now onto debugging

--- src/treeeval.jl
+++ src/treeeval.jl
@@ -15,10 +15,22 @@ using Combinatorics: combinations
 
 import SHIPs: InnerPIBasis
 
-struct TreePIPot{T, NZ, TB} <: SitePotential
+const ETNode{TI} = Tuple{TI,TI}
+
+ETNode{TI}(i1, i2) where {TI} = ( TI(i1), TI(i2) )
+
+
+struct EvalTree{T, TI}
+   nodes::Vector{ETNode{TI}}
+   coeffs::Vector{T}
+   num1::TI
+   numstore::TI
+end
+
+
+struct TreePIPot{T, TI, NZ, TB} <: SitePotential
    basis1p::TB
-   trees::NTuple{NZ, Vector{Tuple{UInt16,UInt16}}}
-   coeffs::NTuple{NZ, Vector{T}}
+   trees::NTuple{NZ, EvalTree{T, TI}}
 end
 
 i2z(V::TreePIPot, i::Integer) = i2z(V.basis1p, i)
@@ -29,30 +41,68 @@ cutoff(V::TreePIPot) = cutoff(V.basis1p)
 
 Base.eltype(V::TreePIPot{T}) where {T} = real(T)
 
+_maxstore(V::TreePIPot) = maximum( tree.numstore for tree in V.trees )
+
+Base.length(tree::EvalTree) = length(tree.nodes)
 
 # ---------------------------------------------------------------------
 #   construction codes
 
 
 function TreePIPot(pipot::PIPotential; kwargs...)
-   trees = [ get_eval_tree(pipot.pibasis.inner[iz]; kwargs...)
-             for iz = 1:numz(pipot) ]
-   return TreePIPot(pipot.pibasis.basis1p, tuple(trees...), pipot.coeffs)
+   trees = [ get_eval_tree(pipot.pibasis.inner[iz], pipot.coeffs[iz];
+                           kwargs...)  for iz = 1:numz(pipot) ]
+   return TreePIPot(pipot.pibasis.basis1p, tuple(trees...))
 end
 
 
 
 
-function get_eval_tree(inner::InnerPIBasis, filter = _->true)
-   tree = Vector{Tuple{UInt16, UInt16}}(undef, length(inner))
+function get_eval_tree(inner::InnerPIBasis, coeffs;
+                       filter = _->true,
+                       TI = UInt16)
    # make a list of all basis functions as vectors so we can search it
-   spec = [ inner.iA2iAA[iAA, 1:inner.orders[iAA]]
+   # TODO: should also check the tuples are sorted lexicographically
+   spec = [ inner.iAA2iA[iAA, 1:inner.orders[iAA]]
             for iAA = 1:length(inner) ]
-   # now look through all of them again
-   for (ikk, kk) in enumerate(spec)
+   @assert issorted(length.(spec))
+   @assert all(issorted, spec)
+   # we need to separate them into 1-p and many-p
+   spec1 = spec[ length.(spec) .== 1 ]
+   coeffs1 = coeffs[1:length(spec1)]
+   IN = (length(spec1)+1):length(spec)
+   specN = spec[IN]
+   coeffsN = coeffs[IN]
+
+   # start assembling the tree
+   nodes = ETNode{TI}[]
+   sizehint!(nodes, length(inner))
+   newcoeffs = Vector{eltype(coeffs)}()
+   sizehint!(newcoeffs, length(inner))
+   specnew = Vector{Int}[]
+   sizehint!(specnew, length(inner))
+
+   # add the full 1-particle basis (N=1) into the tree
+   num1 = maximum(inner.iAA2iA)
+   for i = 1:num1
+      push!(nodes, ETNode{TI}(i, 0))
+      push!(specnew, [i])
+      # find that index in `spec`
+      ispec = findfirst(isequal([i]), spec1)
+      if isnothing(ispec)
+         push!(newcoeffs, 0)
+      else
+         push!(newcoeffs, coeffs1[ispec])
+      end
+   end
+
+   # now we can construct the rest
+   # this is the limit of how many intermediate computations must be stored
+   for (ikk, kk) in enumerate(specN)
+      if length(kk) == 1; continue; end # skip the 1-p, we already have them
       # best length
       bestlen = length(kk)
-      best_node = (UInt16(0), UInt16(0))
+      best_node = ETNode{TI}(0,0)
       for i1 in combinations(1:length(kk))
          if length(i1) > length(kk) ÷ 2; continue; end
          i2 = setdiff(1:length(kk), i1)
@@ -60,20 +110,24 @@ function get_eval_tree(inner::InnerPIBasis, filter = _->true)
          kk2 = sort(kk[i2])
          len = max( length(kk1), length(kk2) )
          if len < bestlen
-            n1 = findfirst(isequal(kk1), spec)
-            n2 = findfirst(isequal(kk2), spec)
+            n1 = findfirst(isequal(kk1), specnew)
+            n2 = findfirst(isequal(kk2), specnew)
             if n1 != nothing && n2 != nothing
                bestlen = len
-               best_node = (UInt16(n1), UInt16(n2))
+               best_node = ETNode{TI}(n1, n2)
             end
          end
       end
-      if best_node == (UInt16(0), UInt16(0))
+      if best_node == ETNode{TI}(0,0)
          error("couldn't find a decomposition for $kk")
       end
-      tree[ikk] = best_node
+      # add into the collection
+      push!(nodes, best_node)
+      push!(newcoeffs, coeffs[ikk])
+      push!(specnew, kk)
    end
-   return tree
+   numstore = maximum(maximum.(nodes))
+   return EvalTree(nodes, newcoeffs, TI(num1), TI(numstore))
 end
 
 
@@ -88,44 +142,46 @@ alloc_temp(V::TreePIPot{T}, maxN::Integer) where {T} =
    Z = zeros(AtomicNumber, maxN),
    tmp_basis1p = alloc_temp(V.basis1p),
    A = alloc_B(V.basis1p),
-   AA = zeros(eltype(V.basis1p), length(V.coeffs))
+   AA = zeros(eltype(V.basis1p), _maxstore(V))
     )
 
 
 function evaluate!(tmp, V::TreePIPot, Rs, Zs, z0)
-   iz0 = z2i(z0)
+   iz0 = z2i(V, z0)
    AA = tmp.AA
-   c = V.coeffs[iz0]
    tree = V.trees[iz0]
+   nodes = tree.nodes
+   c = tree.coeffs
 
    # evaluate the 1-particle basis
-   evaluate!(AA, tmp.tmp_basis1p, Rs, Zs, z0)
+   # this puts the first `tree.num1` 1-b (trivial) correlations into the
+   # storage array, and from these we can build the rest
+   evaluate!(AA, tmp.tmp_basis1p, V.basis1p, Rs, Zs, z0)
 
    # Stage 1: accumulate the first basis functions
    Es = zero(eltype(V))
-   i1 = length(V, z0)
-   for i = 1:i1
-      Es = muladd(c[i], AA[i], Es)
+   for i = 1:tree.num1
+      Es = real(muladd(c[i], AA[i], Es))
    end
 
-   # Stage 2: now go through the tree up to length(AA)
-   #  TODO: this length should maybe be stored!
-   i2 = length(AA)
-   tree = V.trees[iz0]
-   for i = i1+1:i2
-      t = tree[i]
+   # Stage 2: now go through the tree and store the intermediate results we need
+   for i = (tree.num1+1):tree.numstore
+      t = nodes[i]
       a = AA[t[1]] * AA[t[2]]
       AA[i] = a
-      Es = muladd(c[i], a, Es)
+      Es = real(muladd(c[i], a, Es))
    end
 
    # Stage 3:
-   i3 = length(V, z0)
-   for i = i2+1:i3
-      t = tree[i]
+   # continue going through the tree, but now we don't need to store
+   # the new correlations since we don't need them anymore later
+   for i = (tree.numstore+1):length(tree)
+      t = nodes[i]
       a = AA[t[1]] * AA[t[2]]
-      Es = muladd(c[i], a, Es)
+      Es = real(muladd(c[i], a, Es))
    end
+
+   return Es
 end
 
 
--- test/experimental/test_treeval.jl
+++ test/experimental/test_treeval.jl
@@ -10,102 +10,25 @@
 
 using SHIPs
 using Printf, Test, LinearAlgebra, JuLIP, JuLIP.Testing
-using JuLIP: evaluate, evaluate_d, evaluate!, evaluate_d!
+using JuLIP: evaluate, evaluate_d, evaluate!, evaluate_d!, alloc_temp
 using BenchmarkTools, StaticArrays
 using Profile
 
 ##
 
 
-basis = SHIPs.Utils.rpi_basis(N = 8, maxdeg = 18)
+basis = SHIPs.Utils.rpi_basis(N = 3, maxdeg = 6)
 length(basis)
 length(basis.pibasis)
 V = SHIPs.Random.randcombine(basis)
 Rs, Zs, z0 = rand_nhd(15, basis.pibasis.basis1p.J)
 tmp = SHIPs.alloc_temp(V, length(Rs))
-
+evaluate!(tmp, V, Rs, Zs, z0)
 # @btime evaluate!($tmp, $V, $Rs, $Zs, $z0);
-
-
-A = real(tmp.tmp_pibasis.A)
-
-function naiveeval(orders, iAA2iA, coeffs, A)
-   V = 0.0
-   @inbounds begin
-      for iAA = 1:size(iAA2iA, 1)
-         v = 1.0
-         for a =  1:orders[iAA]
-            v *= coeffs[iAA] * A[a]
-         end
-         V += v
-      end
-   end
-   return V
-end
-
-
-function faketreeeval(inds, coeffs, A, ::Val{M}, AA = zero(MVector{M, Float64})) where {M}
-   i1 = length(A)
-   V = 0.0
-   @inbounds begin
-      for i = 1:length(A)
-         AA[i] = A[i]
-         V += coeffs[i] * AA[i]
-      end
-      i2 = M
-      for i = i1+1:i2
-         ip = inds[i]
-         aa = AA[ip[1]] * AA[ip[2]]
-         AA[i] = aa
-         V += coeffs[i] * aa
-      end
-      i3 = length(coeffs)
-      for i = i2+1:i3
-         ip = inds[i]
-         V += coeffs[i] * AA[ip[1]] * AA[ip[2]]
-      end
-   end
-   return V
-end
-
-
-orders = V.pibasis.inner[1].orders
-iAA2iA = V.pibasis.inner[1].iAA2iA
-coeffs = V.coeffs[1]
-inds = iAA2iA[:,1]
-AA = zeros(length(inds))
-
-M = 2000
-inds = [ ( Int(rand(1:M)), Int(rand(1:M)) )
-            for i = 1:length(coeffs) ]
-vM = Val(M)
-AAs = zero(MVector{M, Float64})
-AAh = zeros(M)
 
 ##
 
-print("     Naive Eval:")
-@btime naiveeval($orders, $iAA2iA, $coeffs, $A);
-print("Tree Eval alloc:")
-@btime faketreeeval($inds, $coeffs, $A, $vM);
-print("Tree Eval stack:")
-@btime faketreeeval($inds, $coeffs, $A, $vM, $AAs);
-print(" Tree Eval heap:")
-@btime faketreeeval($inds, $coeffs, $A, $vM, $AAh);
+Vtr = SHIPs.Tree.TreePIPot(V)
 
-# ##
-#
-#
-#
-# Profile.clear()
-# @profile begin
-#    for n = 1:100_000; evaluate!(tmp, V, Rs, Zs, z0); end
-# end
-# ##
-#
-# Profile.print()
-#
-# ##
-#
-# using ProfileView
-# ProfileView.view()
+tmptr = alloc_temp(Vtr, length(Rs))
+evaluate!(tmptr, Vtr, Rs, Zs, z0)
