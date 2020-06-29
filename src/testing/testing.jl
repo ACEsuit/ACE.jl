420c885f588118a1fff5e95688abc34be11b4774
Christoph Ortner <c.ortner@warwick.ac.uk>
2020-06-29 09:57:56 -0700

    testing the testing codes

--- src/testing/testing.jl
+++ src/testing/testing.jl
@@ -1,11 +1,16 @@
 
 module Testing
 
+using LinearAlgebra: eigvals, eigen
+import JuLIP.Potentials: F64fun
+import JuLIP: Atoms, bulk, rattle!, positions, energy, forces, JVec,
+              chemical_symbol, mat
+
 include("../extimports.jl")
 include("../shipimports.jl")
 
 include("testmodel.jl")
-# include("testlsq.jl")
+include("testlsq.jl")
 
 
-end 
+end
