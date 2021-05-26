246cf5c1d38339c50a33681e83aaa7c860a223e5
DavKovacs <davkovacs97@gmail.com>
2021-05-26 23:46:16 +0100

    Cleaned up nB_energy and nB_potential

--- src/utils/pure.jl
+++ src/utils/pure.jl
@@ -7,10 +7,10 @@ module Pure
 
 include("../extimports.jl")
 include("../aceimports.jl")
-#using ACE
+
 using JuLIP
 
-export nB_energy
+export nB_energy, nB_potential
 
 function nB_energy(IP::PIPotential, atoms::Atoms, B_order::Int)
   """
@@ -19,28 +19,40 @@ function nB_energy(IP::PIPotential, atoms::Atoms, B_order::Int)
       IP: PIPotential object
       atoms: Atoms object whose energy should be returned
       B_order: Int, specifying the body order (correlation order + 1)
+    Returns: 
+      Contribution of the N-Body basis functions to the energy. 
   """
   korr_ord = B_order - 1
-  # get the correlation orders of the PIbasis ...
-  Ns_pi = zeros(Int, length(IP.pibasis))
-  for iz0 = 1:numz(IP)
-     inner = IP.pibasis.inner[iz0]
-     Ns_pi[inner.AAindices] .= inner.orders
-  end
   Cs = deepcopy(IP.coeffs)
-  coeff_size = [length(c) for c in Cs]
-  ind = 1
-  for i in 1:length(coeff_size)
-    for j in 1:coeff_size[i]
-        if Ns_pi[ind] != korr_ord
-            Cs[i][j] = 0
-        end
-        ind += 1
+  for i = 1:numz(IP), j = 1:length(Cs[i])
+    ord = IP.pibasis.inner[i].orders[j]
+    if ord != korr_ord
+        Cs[i][j] = 0
     end
   end
   IP_new = PIPotential(IP.pibasis, Cs)
   return energy(IP_new, atoms)
+end
 
+function nB_potential(IP::PIPotential, B_order::Int)
+  """
+  This function returns the n-Body part of the ACE basis set
+    Arguments:
+      IP: PIPotential object
+      atoms: Atoms object whose energy should be returned
+      B_order: Int, specifying the body order (correlation order + 1)
+    Returns: 
+      Potential consisting of the N-Body basis functions only. 
+  """
+  korr_ord = B_order - 1
+  Cs = deepcopy(IP.coeffs)
+  for i = 1:numz(IP), j = 1:length(Cs[i])
+    ord = IP.pibasis.inner[i].orders[j]
+    if ord != korr_ord
+        Cs[i][j] = 0
+    end
+  end
+  return PIPotential(IP.pibasis, Cs)
 end
 
 
