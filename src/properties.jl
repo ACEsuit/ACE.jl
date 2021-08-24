b0c1e25d1bef429ae6c773c8ff285a192d2b7d16
Matthias Sachs <msachs@math.duke.edu>
2021-08-24 14:12:24 -0700

    removed option for EuclidianVector to be complex

--- src/properties.jl
+++ src/properties.jl
@@ -60,10 +60,10 @@ end
 
 Base.show(io::IO, φ::Invariant) = print(io, "i($(φ.val))")
 
-isrealB(::Invariant{<: Real}) = true 
-isrealB(::Invariant{<: Complex}) = false 
-isrealAA(::Invariant{<: Real}) = true 
-isrealAA(::Invariant{<: Complex}) = false 
+isrealB(::Invariant{<: Real}) = true
+isrealB(::Invariant{<: Complex}) = false
+isrealAA(::Invariant{<: Real}) = true
+isrealAA(::Invariant{<: Complex}) = false
 
 Invariant{T}() where {T <: Number} = Invariant{T}(zero(T))
 
@@ -90,21 +90,21 @@ _is_l(sym::Symbol) = (string(sym)[1] == 'l')
 _is_m(sym::Symbol) = (string(sym)[1] == 'm')
 
 
-function filter(φ::Invariant, b::Array) 
+function filter(φ::Invariant, b::Array)
    if length(b) <= 1
-      return true 
-   end 
-   suml = 0 
-   summ = 0 
-   for bi in b 
+      return true
+   end
+   suml = 0
+   summ = 0
+   for bi in b
       for (sym, val) in pairs(bi)
          if _is_l(sym)
-            suml += val 
-         elseif _is_m(sym) 
-            summ += val 
-         end 
+            suml += val
+         elseif _is_m(sym)
+            summ += val
+         end
       end
-   end 
+   end
    return iseven(suml) && iszero(summ)
 end
 
@@ -139,23 +139,23 @@ $O(3)$ as
 ```
 where $\cdot$ denotes the standard matrix-vector product.
 """
-struct EuclideanVector{T} <: AbstractProperty
-   val::SVector{3, T}
+struct EuclideanVector{T} <: AbstractProperty where T<:Real
+   val::SVector{3, Complex{T}}
 end
 
 
-real(φ::EuclideanVector) = EuclideanVector(real(φ.val))
-complex(φ::EuclideanVector) = EuclideanVector(complex(φ.val))
+real(φ::EuclideanVector) = EuclideanVector(φ.val)
+complex(φ::EuclideanVector) = EuclideanVector(φ.val)
 complex(::Type{EuclideanVector{T}}) where {T} = EuclideanVector{complex(T)}
 
-# TODO MATTHIAS - IS THIS RIGHT, OR SHOULD IT DEPEND ON T? 
-isrealB(::EuclideanVector) = true 
-isrealAA(::EuclideanVector) = false 
+# TODO MATTHIAS - IS THIS RIGHT, OR SHOULD IT DEPEND ON T?
+isrealB(::EuclideanVector) = true
+isrealAA(::EuclideanVector) = false
 
 
 #fltype(::EuclideanVector{T}) where {T} = T
 
-EuclideanVector{T}() where {T <: Number} = EuclideanVector{T}(zero(SVector{3, T}))
+EuclideanVector{T}() where {T <: Real} = EuclideanVector{T}(zero(SVector{3, Complex{T}}))
 
 EuclideanVector(T::DataType=Float64) = EuclideanVector{T}()
 
@@ -172,13 +172,13 @@ write_dict(φ::EuclideanVector{T}) where {T} =
 
 function read_dict(::Val{:ACE_EuclideanVector}, D::Dict)
    T = read_dict(D["T"])
-   return EuclideanVector{T}(SVector{3, T}(read_dict(D["val"])))
+   return EuclideanVector{T}(SVector{3, Complex{T}}(read_dict(D["val"])))
 end
 
 # differentiation - cf #27
 # *(φ::EuclideanVector, dAA::SVector) = φ.val * dAA'
 
-coco_init(phi::EuclideanVector{CT}, l, m, μ, T, A) where {CT} = (
+coco_init(phi::EuclideanVector{CT}, l, m, μ, T, A) where {CT<:Real} = (
       (l == 1 && abs(m) <= 1 && abs(μ) <= 1)
          ? [EuclideanVector{CT}(rmatrices[m,μ][:,k]) for k=1:3]
          : coco_zeros(phi, l, m, μ, T, A)  )
@@ -218,8 +218,8 @@ end
 # # differentiation - cf #27
 # *(φ::SphericalVector, dAA::SVector) = φ.val * dAA'
 
-isrealB(::SphericalVector) = false 
-isrealAA(::SphericalVector) = false 
+isrealB(::SphericalVector) = false
+isrealAA(::SphericalVector) = false
 
 
 real(φ::SphericalVector) = SphericalVector(real(φ.val), φ._valL)
@@ -318,8 +318,8 @@ end
 
 getL(φ::SphericalMatrix{L1,L2}) where {L1,L2} = L1, L2
 
-isrealB(::SphericalMatrix) = false 
-isrealAA(::SphericalMatrix) = false 
+isrealB(::SphericalMatrix) = false
+isrealAA(::SphericalMatrix) = false
 
 
 # L = 0 -> (0,0)
