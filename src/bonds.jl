d7c287e7b9490d10679f03032c80846f14aae4fd
Matthias Sachs <msachs@math.duke.edu>
2021-10-04 16:57:28 -0700

    Elipsoid bond  environment added

--- src/bonds.jl
+++ src/bonds.jl
@@ -1,35 +1,38 @@
 
 
 
-abstract type BondEnvelope{T} <: B1pMultiplier{T}  end 
+abstract type BondEnvelope{T} <: B1pMultiplier{T}  end
 
 
-struct CylindricalBondEnvelope{T} <: BondEnvelope{T} 
+struct CylindricalBondEnvelope{T} <: BondEnvelope{T}
    r0cut::T
    rcut::T
-   zcut::T 
+   zcut::T
    p0::Int
    pr::Int
    pz::Int
 end
 
-CylindricalBondEnvelope(r0cut, rcut, zcut; p0 = 2, pr = 2, pz = 2) = 
+CylindricalBondEnvelope(r0cut, rcut, zcut; p0 = 2, pr = 2, pz = 2) =
       CylindricalBondEnvelope(r0cut, rcut, zcut, p0, pr, pz)
 
-# we can't call this `evaluate` since that would return a Vector 
-# whereas _inner_evaluate(...) should just give us a value. 
+# we can't call this `evaluate` since that would return a Vector
+# whereas _inner_evaluate(...) should just give us a value.
 function _inner_evaluate(env::BondEnvelope, X::AbstractState)
-   if X.be == :env 
+   if X.be == :env
       return _evaluate_env(env, X)
-   elseif X.be == :bond 
+   elseif X.be == :bond
       return _evaluate_bond(env, X)
    else
       error("invalid X.be value")
    end
 end
 
+Base.filter(env::BondEnvelope, X::AbstractState) =
+         _inner_evaluate(env,X) != 0
 
-Base.filter(env::CylindricalBondEnvelope, X::AbstractState) = 
+
+Base.filter(env::CylindricalBondEnvelope, X::AbstractState) =
          _inner_evaluate(env,X) != 0
 
 
@@ -45,8 +48,67 @@ function _evaluate_env(env::CylindricalBondEnvelope, X::AbstractState)
    r̂b = X.rr0/norm(X.rr0)
    z = dot(X.rr - r_centre, r̂b)
    r = norm( (X.rr - r_centre) - z * r̂b )
-   # then return the correct cutoff 
+   # then return the correct cutoff
    zcuteff = env.zcut + norm(X.rr0) / 2
-   return ( (z/zcuteff)^2 - 1 )^(env.pz) * (abs(z) <= zcuteff) * 
+   return ( (z/zcuteff)^2 - 1 )^(env.pz) * (abs(z) <= zcuteff) *
             ( (r/env.rcut)^2 - 1 )^(env.pr) * (r <= env.rcut)
+end
+
+
+struct DiskBondEnvelope{T} <: BondEnvelope{T}
+   r0cut::T # max bond length
+   rcut::T  # Disck environment cut off radius
+   p0::Int
+   pr::Int
+end
+
+
+function _evaluate_bond(env::DiskBondEnvelope, X::AbstractState)
+   r = norm(X.rr0)
+   return ((r/env.r0cut)^2 - 1)^(env.p0) * (r <= env.r0cut)
+end
+
+
+function _evaluate_env(env::DiskBondEnvelope, X::AbstractState)
+   # convert to cylindrical coordinates
+   r_centre = X.rr0/2
+   r = norm( X.rr - r_centre )
+   # then return the correct cutoff
+   return ((r/env.rcut)^2 - 1.0)^(env.pr) * (r <= env.rcut)
+end
+
+
+
+struct ElipsoidBondEnvelope{T} <: BondEnvelope{T}
+   r0cut::T
+   rcut::T
+   zcut::T  # must satisfy zcut >= r0cut/2
+   p0::Int
+   pr::Int
+   floppy::Bool
+end
+
+ElipsoidBondEnvelope(r0cut, zcut, rcut; p0=2, pr=2, floppy=false) = ElipsoidBondEnvelope(r0cut, zcut, rcut, p0, pr, floppy)
+ElipsoidBondEnvelope(r0cut, cut; p0=2, pr=2, floppy=false) = ElipsoidBondEnvelope(r0cut, cut, cut, p0, pr, floppy)
+function _evaluate_bond(env::ElipsoidBondEnvelope, X::AbstractState)
+   r = norm(X.rr0)
+   return ((r/env.r0cut)^2 - 1)^(env.p0) * (r <= env.r0cut)
+end
+
+function _evaluate_env(env::ElipsoidBondEnvelope, X::AbstractState)
+   # convert to cylindrical coordinates
+   x_centre = X.rr0/2
+   x̃ = X.rr - x_centre
+   if norm(X.rr0) > 0
+      x_centre_0  = X.rr0/norm(X.rr0)
+      z̃ = dot(x_centre_0, x̃ )
+      r̃ = norm( x̃ - z̃ * x_centre_0)
+   else
+      z̃ = 0.0
+      r̃ = norm(x̃)
+   end
+   # then return the correct cutoff
+   zeff = env.zcut + env.floppy * norm(x_centre)
+   g = (z̃/zeff)^2 +(r̃/env.rcut)^2
+   return ( g - 1.0)^(env.pr) * (g <= 1)
 end
--- test/test_bonds.jl
+++ test/test_bonds.jl
@@ -1,6 +1,6 @@
 using ACE, Test, LinearAlgebra
 using ACE: State, CylindricalBondEnvelope
-using StaticArrays 
+using StaticArrays
 using ACEbase.Testing: print_tf
 
 ## Use a specific example to test the Cylindrical cutoff
@@ -16,7 +16,7 @@ env = CylindricalBondEnvelope(r0cut, rcut, zcut)
 
 for i = 1:30
     rr = rand(SVector{3, Float64}) * 2 * r0cut / sqrt(3)
-    r = norm(rr) 
+    r = norm(rr)
     X = State(rr = rr, rr0 = rr, be=:bond)
     print_tf(@test( filter(env, X) == (r <= r0cut) ))
 end
@@ -33,10 +33,53 @@ for i = 1:30
     X = State(rr = rr, rr0 = r0, be=:env)
     z = rr[1] - r_centre[1]
     r = sqrt(rr[2]^2 + rr[3]^2)
-    filt = (abs(z) <= env.zcut+r_centre[1]) * (r <= env.rcut) 
+    filt = (abs(z) <= env.zcut+r_centre[1]) * (r <= env.rcut)
     print_tf(@test( filter(env, X) == (filt != 0) ))
 
     zeff = env.zcut + norm(r_centre)
     val = ((r/env.rcut)^2 - 1)^env.pr * ( (z/zeff)^2 - 1 )^env.pz * filt
     print_tf(@test( ACE._inner_evaluate(env, X) ≈ val ))
+end
+
+
+#%%
+
+@info("Testing Elipsoid Bond Envelope")
+
+
+r0cut = 2.0
+rcut = 1.0
+zcut = 2.0
+for floppy = [false, true]
+    env = ElipsoidBondEnvelope(r0cut, rcut, zcut;floppy=floppy)
+
+    @info("Test :bond", floppy)
+
+    for i = 1:30
+        rr = rand(SVector{3, Float64}) * 2 * r0cut / sqrt(3)
+        r = norm(rr)
+        X = State(rr = rr, rr0 = rr, be=:bond)
+        print_tf(@test( filter(env, X) == (r <= r0cut) ))
+    end
+
+    ##
+
+
+    @info ("Test :env")
+
+    r0 = @SVector [r0cut/2, 0.0, 0.0]
+    r_centre = r0 / 2
+    for i = 1:30
+        rr = rand(SVector{3, Float64}) * env.rcut + r_centre
+        X = State(rr = rr, rr0 = r0, be=:env)
+        z = rr[1] - r_centre[1]
+        r = sqrt(rr[2]^2 + rr[3]^2)
+        zeff = env.zcut + env.floppy*norm(r_centre)
+
+        filt = (((z/ zeff)^2 + (r/env.rcut)^2) <=1)
+        print_tf(@test( filter(env, X) == (filt != 0) ))
+
+        val = ( (z/zeff)^2 +  (r/env.rcut)^2 - 1.0)^env.pr * filt
+        print_tf(@test( ACE._inner_evaluate(env, X) ≈ val ))
+    end
 end
