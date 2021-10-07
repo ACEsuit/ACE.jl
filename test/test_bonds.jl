67c1904d401bc8e7fb5e78d1d640c9a2f5a52181
Matthias Sachs <msachs@math.ubc.ca>
2021-10-06 18:09:59 -0700

    Update test_bonds.jl
    whitespace

--- test/test_bonds.jl
+++ test/test_bonds.jl
@@ -17,7 +17,7 @@ env = CylindricalBondEnvelope(r0cut, rcut, zcut)
 
 for i = 1:30
     rr = rand(SVector{3, Float64}) * 2 * r0cut / sqrt(3) 
-    r = norm(rr)
+    r = norm(rr) 
     X = State(rr = rr, rr0 = rr, be=:bond)
     print_tf(@test( filter(env, X) == (r <= r0cut) ))
 end
@@ -33,8 +33,8 @@ for i = 1:30
     rr = rand(SVector{3, Float64}) * env.rcut + r_centre
     X = State(rr = rr, rr0 = r0, be=:env)
     z = rr[1] - r_centre[1]
-    r = sqrt(rr[2]^2 + rr[3]^2) 
-    filt = (abs(z) <= env.zcut+r_centre[1]) * (r <= env.rcut)
+    r = sqrt(rr[2]^2 + rr[3]^2)  
+    filt = (abs(z) <= env.zcut+r_centre[1]) * (r <= env.rcut) 
     print_tf(@test( filter(env, X) == (filt != 0) ))
 
     zeff = env.zcut + norm(r_centre)
