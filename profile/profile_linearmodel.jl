a28d0e30aec6a9f324c27ce2731757b6747af76f
Christoph Ortner <christophortner0@gmail.com>
2021-12-26 17:52:46 -0800

    minor improvements to pi basis grad_config

--- profile/profile_linearmodel.jl
+++ profile/profile_linearmodel.jl
@@ -5,16 +5,19 @@ using Printf, LinearAlgebra, ACE.Testing, Random
 using ACE: evaluate, evaluate_d, SymmetricBasis, SimpleSparseBasis, PIBasis, 
            PositionState
 using ACEbase.Testing: fdtest
-using Profile, ProfileView, TimerOutputs, BenchmarkTools
+using Profile, ProfileSVG, TimerOutputs, BenchmarkTools
 
 ##
 
 # construct the 1p-basis
 maxdeg = 14
 ord = 4
-Bsel = SimpleSparseBasis(ord, maxdeg)
+wL = 1.5
+Bsel = SparseBasis(; maxorder = ord, p = 1, default_maxdeg = maxdeg, 
+                     weight = Dict(:n => 1.0, :l => wL))
 
-B1p = ACE.Utils.RnYlm_1pbasis(; maxdeg=maxdeg, Bsel=Bsel)
+B1p = ACE.Utils.RnYlm_1pbasis(maxdeg = maxdeg, maxL = ceil(Int, maxdeg/wL), 
+                              Bsel = Bsel)
 
 # generate a configuration
 nX = 10
@@ -33,23 +36,28 @@ standard = ACE.LinearACEModel(basis, c, evaluator = :standard)
 ##
 
 @info("Time evaluate incl allocation")
-@btime evaluate($standard, $cfg)
+(@btime evaluate($standard, $cfg))
 
 ##
 
 @info("Time grad_config with and without allocation")
 @info("This looks like a factor 4.5 of evaluate, so probably more we can do")
 g = ACE.acquire_grad_config!(standard, cfg)
-@btime ACE.grad_config($standard, $cfg)
-@btime ACE.grad_config!($g, $standard, $cfg)
+(@btime ACE.grad_config($standard, $cfg))
+(@btime ACE.grad_config!($g, $standard, $cfg))
+
+# [ Info: Time grad_config with and without allocation
+# [ Info: This looks like a factor 4.5 of evaluate, so probably more we can do
+#   283.584 μs (6 allocations: 275.34 KiB)
+#   283.458 μs (5 allocations: 275.05 KiB)
 
 ##
 
 @info("Time grad_params with and without allocation")
 @info("a little surprising we dont get closer to factor 1?")
 g = ACE.acquire_grad_params!(standard, cfg)
-@btime ACE.grad_params($standard, $cfg)
-@btime ACE.grad_params!($g, $standard, $cfg)
+(@btime ACE.grad_params($standard, $cfg))
+(@btime ACE.grad_params!($g, $standard, $cfg))
 
 
 ##
@@ -64,11 +72,16 @@ end
 
 ##
 
-# Profile.clear()
-# @profile runn(100, ACE.grad_config, standard, cfg);
-# Profile.print()
+Profile.clear()
+@profile runn(20_000, ACE.grad_config, standard, cfg);
+Profile.print()
 
-# ##
+##
+
+ProfileSVG.view()
+
+
+##
 
 # # make sure to add suitable @timeit macros to the module
 # @info("Detailed benchmarking of grad_config")
--- src/pibasis.jl
+++ src/pibasis.jl
@@ -270,22 +270,85 @@ function evaluate_ed!(AA, dAA, basis::PIBasis,
 end
 
 function _AA_local_adjoints!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   if ord == 1
+      return _AA_local_adjoints_1!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   elseif ord == 2
+      return _AA_local_adjoints_2!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   # elseif ord == 3
+   #    return _AA_local_adjoints_3!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   # elseif ord == 4
+   #    return _AA_local_adjoints_4!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   else 
+      return _AA_local_adjoints_x!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   end
+end
+
+function _AA_local_adjoints_1!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   @inbounds dAAdA[1] = 1 
+   @inbounds A1 = A[iAA2iA[iAA, 1]]
+   return _real(A1)
+end
+
+function _AA_local_adjoints_2!(dAAdA, A, iAA2iA, iAA, ord, _real)
+   @inbounds A1 = A[iAA2iA[iAA, 1]]
+   @inbounds A2 = A[iAA2iA[iAA, 2]]
+   @inbounds dAAdA[1] = A2 
+   @inbounds dAAdA[2] = A1
+   return _real(A1 * A2)
+end
+
+# function _AA_local_adjoints_3!(dAAdA, A, iAA2iA, iAA, ord, _real)
+#    A1 = A[iAA2iA[iAA, 1]]
+#    A2 = A[iAA2iA[iAA, 2]]
+#    A3 = A[iAA2iA[iAA, 3]]
+#    A12 = A1 * A2
+#    dAAdA[1] = A2 * A3  
+#    dAAdA[2] = A1 * A3 
+#    dAAdA[3] = A12
+#    return _real(A12 * A3)
+# end
+
+# function _AA_local_adjoints_4!(dAAdA, A, iAA2iA, iAA, ord, _real)
+#    @inbounds A1 = A[iAA2iA[iAA, 1]]
+#    @inbounds A2 = A[iAA2iA[iAA, 2]]
+#    @inbounds A3 = A[iAA2iA[iAA, 3]]
+#    @inbounds A4 = A[iAA2iA[iAA, 4]]
+#    A12 = A1 * A2
+#    A34 = A3 * A4
+#    @inbounds dAAdA[1] = A2 * A34  
+#    @inbounds dAAdA[2] = A1 * A34 
+#    @inbounds dAAdA[3] = A12 * A4 
+#    @inbounds dAAdA[4] = A12 * A3
+#    return _real(A12 * A34)
+# end
+
+
+function _AA_local_adjoints_x!(dAAdA, A, iAA2iA, iAA, ord, _real)
    @assert length(dAAdA) >= ord
+   @assert ord >= 2
    # TODO - optimize a bit more? can move one operation out of the loop
    # Forward pass:
+   @inbounds A1 = A[iAA2iA[iAA, 1]]
+   @inbounds A2 = A[iAA2iA[iAA, 2]]
    @inbounds dAAdA[1] = 1
-   @inbounds AAfwd = A[iAA2iA[iAA, 1]]
-   @inbounds for a = 2:ord
+   @inbounds dAAdA[2] = A1
+   @inbounds AAfwd = A1 * A2
+   @inbounds for a = 3:ord-1
       dAAdA[a] = AAfwd
       AAfwd *= A[iAA2iA[iAA, a]]
    end
-   aa = _real(AAfwd)
+   @inbounds dAAdA[ord] = AAfwd
+   @inbounds Aend = A[iAA2iA[iAA, ord]]
+   aa = _real(AAfwd * Aend)
    # backward pass
-   @inbounds AAbwd = A[iAA2iA[iAA, ord]]
-   @inbounds for a = ord-1:-1:1
+   @inbounds AAbwd = Aend 
+   @inbounds for a = ord-1:-1:3
       dAAdA[a] *= AAbwd
       AAbwd *= A[iAA2iA[iAA, a]]
    end
+   dAAdA[2] *= AAbwd 
+   AAbwd *= A2 
+   dAAdA[1] *= AAbwd 
 
    return aa 
 end
