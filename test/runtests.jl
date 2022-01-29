28ac1fb496d4730ef26c4658f3c89c0b2b0ce886
Matthias Sachs <msachs@math.ubc.ca>
2022-01-29 13:09:04 +0000

    several changes
    -new filter added to basis selectors
    
    - utils.SymmetricBond_basis extended to include symmetry option for bond sign flip
    -  renamed "test_basisselectors.jl"

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,7 +38,7 @@ using ACE, Test, Printf, LinearAlgebra, StaticArrays, BenchmarkTools
     # Experimental material
     @testset "Multipliers" begin include("test_multiplier.jl") end
     @testset "Bonds" begin include("test_bonds.jl") end
-    @testset "BasisSelectors" begin include("test_basisselectors.jl") end
+    @testset "Bond BasisSelectors" begin include("test_bondbasisselectors.jl") end
 end
 
 
